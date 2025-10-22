884afd91f5a9fc7cc9a063e6101c7b8c5469e38b
Feodor Fitsner <feodor@appveyor.com>
2025-10-22 14:20:31 -0700

    Add integration test and update dependencies
    Introduced an integration test in app_test.dart for end-to-end testing. Updated pubspec.yaml to switch serious_python to the main branch and added integration_test and flet_integration_test to dev_dependencies.

---
+++ {{cookiecutter.out_dir}}/integration_test/app_test.dart
@@ -0,0 +1,55 @@
+import 'package:flet_integration_test/flet_integration_test.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:{{ cookiecutter.project_name }}/main.dart' as app;
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('test app', (tester) async {
+      const testerServerUrl = String.fromEnvironment('FLET_TEST_SERVER_URL');
+      late FlutterWidgetTester widgetTester;
+
+      if (testerServerUrl.isNotEmpty) {
+        debugPrint('Connecting to remote tester at $testerServerUrl');
+        widgetTester = await RemoteWidgetTester.connect(
+          tester: tester,
+          binding: binding,
+          serverUri: Uri.parse(testerServerUrl),
+        );
+      } else {
+        widgetTester = FlutterWidgetTester(tester, binding);
+      }
+
+      app.tester = widgetTester;
+
+      final args = <String>[];
+
+      const fletTestAppUrl = String.fromEnvironment('FLET_TEST_APP_URL');
+      if (fletTestAppUrl.isNotEmpty) {
+        args.add(fletTestAppUrl);
+      }
+
+      const fletTestPidFile = String.fromEnvironment('FLET_TEST_PID_FILE_PATH');
+      if (fletTestPidFile.isNotEmpty) {
+        args.add(fletTestPidFile);
+      }
+
+      const fletTestAssetsDir = String.fromEnvironment('FLET_TEST_ASSETS_DIR');
+      if (fletTestAssetsDir.isNotEmpty) {
+        args.add(fletTestAssetsDir);
+      }
+
+      app.main(args);
+
+      await Future<void>.delayed(const Duration(milliseconds: 500));
+      await widgetTester.pump(duration: const Duration(seconds: 1));
+      await widgetTester
+          .pumpAndSettle(duration: const Duration(milliseconds: 100));
+      await widgetTester.waitForTeardown();
+    });
+  });
+}
